32fc6cdab7a48e7740a28818313f1ed413e92feb
abdullah mohamed <abdullah2662888@gmail.com>
2025-02-06 15:35:01 +0200

    test

--- database/migrations/2024_09_08_045712_create_wallets_table.php
+++ database/migrations/2024_09_08_045712_create_wallets_table.php
@@ -23,8 +23,6 @@ return new class extends Migration
       $table->string('amount');
       $table->timestamps();
     });
-
-    DB::statement('ALTER TABLE wallets AUTO_INCREMENT = 2000');
   }
 
   /**
